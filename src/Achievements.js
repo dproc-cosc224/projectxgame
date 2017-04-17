2ac289c88d67013fe6eed799fcc25bc1d322a7ca
Martin Wallace <martin.v.wallace@ieee.org>
2017-04-17 09:37:20 -0700

    Add base achievements page

--- src/Achievements.js
+++ src/Achievements.js
@@ -10,15 +10,19 @@ Game.Achievements = function(game){
 
 Game.Achievements.prototype = {
 
-    init: function(){
-
-    },
-
-    preload: function(){
-
-    },
-
     create: function(){
+        var fontAchievements = { font: "32px Arial", fill: "000" };
+        var textAchievements = this.add.text(100, 75, 'Achievements screen', fontAchievements);
 
+        var buttonBack = this.add.button(this.world.width-20,
+            game.world.height-20, 'button-back', this.clickBack, this, 1, 0, 2);
+
+        buttonBack.anchor.set(1, 1);
+        buttonBack.x = this.world.width+buttonBack.width+20;
+        this.add.tween(buttonBack).to({x: this.world.width-20}, 500, Phaser.Easing.Exponential.Out, true);
+    }
+
+    clickBack: function() {
+        this.game.state.start('MainMenu');
     }
 };
